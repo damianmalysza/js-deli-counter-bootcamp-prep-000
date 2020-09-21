2f33f13a321104f94e4ebb0cbf239968b0c6ff15
Damian Malysza <damian.malysza@gmail.com>
2020-09-21 02:51:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ function currentLine(line){
   if (line.length===0){
     return "The line is currently empty."
   } else {
-    
+    var crntLine="The line is currently: "
+    for (let i=0;i<line.length;i++){
+      position=line.indexOf+1
+      var crntLine+=`${position}. ${line[i]}, `
+    }
   }
+  return crntLine
 }
