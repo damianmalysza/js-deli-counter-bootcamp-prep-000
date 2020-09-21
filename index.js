bf8f2d9b0bac829d5b7cf6f87e030b4f81eabdde
Damian Malysza <damian.malysza@gmail.com>
2020-09-21 02:46:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ function nowServing(line){
     return `Currently serving ${first}.`
   }
 }
+
+function currentLine(line){
+  if (line.length===0){
+    return "The line is currently empty."
+  } else {
+    
+  }
+}
